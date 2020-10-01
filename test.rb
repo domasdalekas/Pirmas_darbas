dc2c78bb32100adc12845396f729f9669b2724e5
domasdalekas <48487114+domasdalekas@users.noreply.github.com>
2020-10-01 16:35:59 +0300

    prideti komentarai

--- test.rb
+++ test.rb
@@ -7,10 +7,10 @@ puts 'Iveskite triju krastiniu ilgius'
 a=gets.chomp.to_i
 b=gets.chomp.to_i
 c=gets.chomp.to_i
-p=(a+b+c)/2
-plotas=Math.sqrt(p*(p-a)*(p-b)*(p-c))
+p=(a+b+c)/2 #apskaiciuojamas pusperimetris
+plotas=Math.sqrt(p*(p-a)*(p-b)*(p-c)) # naudojama formule apskaiciuoti plotui
 puts plotas
-if(a+b>c || a+c>b || c+b>a )
+if(a+b>c || a+c>b || c+b>a ) #salyga ar trikampis egzistuoja
     puts"trikampis egzistuoja"
 
 
